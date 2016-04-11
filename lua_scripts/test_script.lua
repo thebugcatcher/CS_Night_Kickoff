b4320a1724c4c28fd6be48e6243faa2da54711b7
aditya7iyengar <adi.iyengar@annkissam.com>
2016-04-11 16:25:41 -0400

    Update script for title match

--- lua_scripts/test_script.lua
+++ lua_scripts/test_script.lua
@@ -4,4 +4,9 @@ scriptDetailsUrl = ""
 
 function onForegroundWindowChange(app, title)
     myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    local titleMatch = string.match(title, "Race The Sun") ~= nil or string.match(title, "RaceTheSun") ~= nil;
+    myo.debug("Race the Sun: "  .. tostring(titleMatch))
+    if (titleMatch) then
+        myo.setLockingPolicy("none")
+    end
 end

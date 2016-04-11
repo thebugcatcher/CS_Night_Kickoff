c21a94dc22a6f350f60f548865afa0eb3b4a9ee8
aditya7iyengar <adi.iyengar@annkissam.com>
2016-04-11 15:26:21 -0400

    Add test script

---
+++ lua_scripts/test_script.lua
@@ -0,0 +1,7 @@
+scriptId = 'com.thalmic.examples.test'
+scriptTitle = "test"
+scriptDetailsUrl = ""
+
+function onForegroundWindowChange(app, title)
+    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+end
